ed81268e611560cc1427c4ae117ac90fc3a7cdc4
Stefan Nestorovic <stefpepp@gmail.com>
2022-12-02 13:09:46 +0100

    Add terraform files, to automatically deploy to aws s3 and cloudfront

---
+++ infrastructure/variables.tf
@@ -0,0 +1,45 @@
+variable "prefix" {
+  default = "cinema-react-opasni"
+}
+
+variable "project" {
+  default = "cinema-app"
+}
+
+variable "custom_error_response" {
+  type = list(object({
+    error_caching_min_ttl = number
+    error_code            = number
+    response_code         = number
+    response_page_path    = string
+  }))
+
+  description = " List of one or more custom error response element maps"
+
+  default = [
+    {
+      error_caching_min_ttl = 10
+      error_code            = 400
+      response_code         = 200
+      response_page_path    = "/index.html"
+    },
+    {
+      error_caching_min_ttl = 10
+      error_code            = 403
+      response_code         = 200
+      response_page_path    = "/index.html"
+    },
+    {
+      error_caching_min_ttl = 10
+      error_code            = 404
+      response_code         = 200
+      response_page_path    = "/index.html"
+    },
+    {
+      error_caching_min_ttl = 10
+      error_code            = 405
+      response_code         = 200
+      response_page_path    = "/index.html"
+    }
+  ]
+}
