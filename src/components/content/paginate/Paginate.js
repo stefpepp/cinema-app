30257c110ceb97ce227c5d36d7783173bead387a
Stefan Nestorovic <stefpepp@gmail.com>
2023-02-07 16:06:41 +0100

    Paginate, grid and rating components

---
+++ src/components/content/paginate/Paginate.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import './Paginate.scss';
+
+const Paginate = (props) => {
+  const { currentPage, totalPages, paginate } = props;
+  const [page, setPage] = useState();
+  const [totalPageNumber, setTotalPageNumber] = useState();
+
+  useEffect(() => {
+    setPage(currentPage);
+    setTotalPageNumber(totalPages);
+  }, [currentPage, totalPages]);
+  console.log('page,', page, 'curr,', currentPage, 'total,', totalPageNumber);
+  return (
+    <>
+      <span className="pageCount">
+        {page} - {totalPageNumber}
+      </span>
+      <button className={page > 1 ? 'paginate-button' : 'paginate-button disable'} onClick={() => paginate('prev')}>
+        Prev
+      </button>
+      <button className={page !== totalPageNumber ? 'paginate-button' : 'paginate-button disable'} onClick={() => paginate('next')}>
+        Next
+      </button>
+    </>
+  );
+};
+
+Paginate.propTypes = {
+  currentPage: PropTypes.number.isRequired,
+  totalPages: PropTypes.number.isRequired,
+  paginate: PropTypes.func.isRequired
+};
+
+export default Paginate;
